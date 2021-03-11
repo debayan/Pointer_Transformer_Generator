b68648498563c15351cc8cdea1bd7611b33e3f4c
Debayan Banerjee <debayanin@gmail.com>
2021-03-11 23:07:31 +0100

    rels only

--- entitybatcher.py
+++ entitybatcher.py
@@ -28,10 +28,10 @@ def example_generator(filename, vocab_path, vocab_size, max_enc_len, max_dec_len
                          
                         questiontokens = linearr[2]
                         sepindices = [i for i, x in enumerate(questiontokens) if x == '[SEP]']
+                        entsep = sepindices[-2]
                         relsep = sepindices[-1]
-                        questiontokens = questiontokens[:relsep]
-                        #questiontokens = questiontokens[:idxrem]
-                        questionvectors = linearr[3][:relsep]#[:idxrem]
+                        questiontokens = questiontokens[:entsep]+questiontokens[relsep:]
+                        questionvectors = linearr[3][:entsep]+linearr[3][relsep:]
                         ents = linearr[4]
                         rels = linearr[5]
                         finents = linearr[6]
