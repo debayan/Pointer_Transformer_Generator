0162bae269cf3ec7b414bdf14bd53d97198e205c
Debayan Banerjee <debayanin@gmail.com>
2021-05-16 01:17:54 +0200

    snapshot

---
+++ pointernetwork/lcq1jivat800/kbstats3253_corrctor.py
@@ -0,0 +1,170 @@
+import sys,os,json,rdflib,re,copy,requests
+from ptrcorrector import convex_hull 
+
+
+def calcf1(target,answer):
+    if empty(target):
+        return 0.0
+    if target == answer:
+        return 1.0
+    try:
+        tb = target['results']['bindings']
+        rb = answer['results']['bindings']
+        tp = 0
+        fp = 0
+        fn = 0
+        for r in rb:
+            if r in tb:
+                tp += 1
+            else:
+                fp += 1
+        for t in tb:
+            if t not in rb:
+                fn += 1
+        precision = tp/float(tp+fp+0.001)
+        recall = tp/float(tp+fn+0.001)
+        f1 = 2*(precision*recall)/(precision+recall+0.001)
+        print("f1: ",f1)
+        return f1
+    except Exception as err:
+        print(err)
+    try:
+        if target['boolean'] == answer['boolean']:
+            print("boolean true/false match")
+            f1 = 1.0
+            print("f1: ",f1)
+            return f1
+        if target['boolean'] != answer['boolean']:
+            print("boolean true/false mismatch")
+            f1 = 0.0
+            print("f1: ",f1)
+            return f1
+    except Exception as err:
+        f1 = 0.0
+        print("f1: ",f1)
+        return f1 
+        
+             
+def hitkg(query,typeq):
+    try:
+        url = 'http://ltcpu1:8892/sparql/'
+        print(query)
+        r = requests.get(url, params={'format': 'json', 'query': query})
+        json_format = r.json()
+        #print(entid,json_format)
+        results = json_format
+        if not results and typeq == 'target':
+            print("no response")
+            sys.exit(1)
+        return results
+    except Exception as err:
+        print(err)
+        if typeq == 'target':
+            print("no response on target")
+            #sys.exit(1)
+        return ''
+
+def replace(query,ents,rels):
+    queryout = query
+    for idx1,ent in enumerate(ents):
+        if ent:
+            queryout = queryout.replace('entpos@@'+str(idx1+1),ent)
+    for idx1,rel in enumerate(rels):
+        if rel:
+            queryout = queryout.replace('predpos@@'+str(idx1+1),rel)
+    return queryout
+
+def empty(r):
+    if not r:
+        return True
+    if 'boolean' not in r:
+        if 'results' in r:
+            if 'bindings' in r['results']:
+                if not r['results']['bindings']:
+                    return True
+    return False
+
+
+def start(filename, modelnum):
+    d = json.loads(open(filename).read()) #eg: model_folder_test31.1out.json
+    ch = convex_hull.ConvexHull(modelnum)    
+    em = 0
+    nem = 0
+    qem = 0
+    qnem = 0
+    totf1 = 0.0
+    testqcount = 0
+    for idx,item in enumerate(d):
+        print(item)
+        print(str(item['uid']))
+        print(item['question'])
+        target = item['target']
+        answer = item['answer_0']
+        ents = item['goldents']
+        rels = item['goldrels']
+        print("ents: ",ents)
+        print("rels: ",rels)
+        print("target: ",target)
+        print("answer: ",answer)
+        if target == answer:
+            qem += 1
+            print("querymatch")
+        else:
+            qnem += 1
+            print("querynotmatch")
+        targ_ = target#item['querytemptar']
+        ans_ = answer#item['querytempans']
+        target = replace(target,ents,rels)
+        print("replaced: ",target)
+        answer = replace(answer,ents,rels)
+        print("replaced: ",answer)
+        resulttarget = hitkg(target,'target')
+        resultanswer = hitkg(answer,'answer')
+        #wans_ = item['question'][0] + ' ' + ans_
+        if empty(resultanswer):
+            print("no answer")
+            print("sending to corrector: ",ans_)
+            for query in ch.correct(ans_):
+                print("alternatequery: ", query)
+                q = replace(query,ents,rels)
+                print("replaces: ",q)
+                try:
+                    res = hitkg(q,'answer')
+                    print("result: ",res)
+                except Exception as err:
+                    print(err)
+                    continue
+                if not empty(res):
+                    resultanswer = res
+                    break
+        f1  = calcf1(resulttarget,resultanswer)
+        totf1 += f1
+        avgf1 = totf1/float(idx+1)
+        if resulttarget == resultanswer:
+            print("match")
+            em += 1
+        else:
+            print("nomatch")
+            nem += 1
+        print("target_filled: ",target)
+        print("answer_filled: ",answer)
+    #    print("original_quer: ",ques3253[item['uid']])
+        print("gold: ",resulttarget)
+        print("result: ",resultanswer)
+        print('................')
+        print("exactmatch: ",em, "  notmatch: ",nem," total: ",idx)
+        print("querymatch: ",qem," querynotmatch: ",qnem)
+        print("avg f1: ",avgf1)
+    return avgf1
+
+def f1eval(filename, modelnum):
+    try:
+        return start(filename, modelnum)
+    except Exception as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    modelnum = sys.argv[2]
+    f1eval(filename, modelnum)
---
+++ pointernetwork/lcq1jivat800/ptrcorrector/convex_hull.py
@@ -0,0 +1,143 @@
+import tensorflow as tf
+import numpy as np
+import ptrcorrector.pointer_net as pointer_net
+import time
+import os
+import json
+import sys
+
+tf.app.flags.DEFINE_integer("batch_size1", 20,"Batch size.")
+tf.app.flags.DEFINE_integer("max_input_sequence_len1", 500, "Maximum input sequence length.")
+tf.app.flags.DEFINE_integer("max_output_sequence_len1", 500, "Maximum output sequence length.")
+tf.app.flags.DEFINE_integer("rnn_size1", 32, "RNN unit size.")
+tf.app.flags.DEFINE_integer("attention_size1", 50, "Attention size.")
+tf.app.flags.DEFINE_integer("num_layers1", 1, "Number of layers.")
+tf.app.flags.DEFINE_integer("beam_width1", 10, "Width of beam search .")
+tf.app.flags.DEFINE_float("learning_rate1", 0.001, "Learning rate.")
+tf.app.flags.DEFINE_float("max_gradient_norm1", 5.0, "Maximum gradient norm.")
+tf.app.flags.DEFINE_boolean("forward_only1", False, "Forward Only.")
+#tf.app.flags.DEFINE_string("log_dir", "./log", "Log directory")
+tf.app.flags.DEFINE_string("data_path1", "./train.txt", "Data path.")
+tf.app.flags.DEFINE_string("test_data_path1", "./test.txt", "Data path.")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint1", 200, "frequence to do per checkpoint.")
+
+FLAGS = tf.app.flags.FLAGS
+#FLAGS.log_dir1='./'
+
+class ConvexHull(object):
+  def __init__(self, modelnum):
+    self.modelnum = modelnum
+    self.testgraph = tf.Graph()
+    with self.testgraph.as_default():
+      config = tf.ConfigProto()
+      config.gpu_options.allow_growth = True
+      config.operation_timeout_in_ms=6000
+      self.testsess = tf.Session(config=config)
+    self.build_model()
+    #self.read_data()
+    #self.test_read_data()
+    self.id2word = json.loads(open('id2word_lcq13253_jivat_w_'+modelnum+'.txt').read())
+    self.word2id = json.loads(open('word2id_lcq13253_jivat_w_'+modelnum+'.txt').read())
+    
+
+  def vectorise(self,tokenids):
+    print("tokenids: ",tokenids)
+    inputs = []
+    enc_input_weights = []
+    enc_input = []
+    for t in tokenids:
+      enc_input.append(int(t))
+    enc_input_len = len(enc_input)
+    enc_input += [0]*((FLAGS.max_input_sequence_len1-enc_input_len))
+    enc_input = np.array(enc_input).reshape([-1,1])
+    inputs.append(enc_input)
+    weight = np.zeros(FLAGS.max_input_sequence_len1)
+    weight[:enc_input_len]=1
+    enc_input_weights.append(weight)
+    self.test_inputs = np.stack(inputs)
+    self.test_enc_input_weights = np.stack(enc_input_weights)
+    print("Load inputs:            " +str(self.test_inputs.shape))
+    print("Load enc_input_weights: " +str(self.test_enc_input_weights.shape))
+
+  def build_model(self):
+    with self.testgraph.as_default():
+      self.testmodel = pointer_net.PointerNet(batch_size=1,
+                    max_input_sequence_len=FLAGS.max_input_sequence_len1,
+                    max_output_sequence_len=FLAGS.max_output_sequence_len1,
+                    rnn_size=FLAGS.rnn_size1,
+                    attention_size=FLAGS.attention_size1,
+                    num_layers=FLAGS.num_layers1,
+                    beam_width=FLAGS.beam_width1,
+                    learning_rate=FLAGS.learning_rate1,
+                    max_gradient_norm=FLAGS.max_gradient_norm1,
+                    forward_only=True)
+
+      # Prepare Summary writer
+      # Try to get checkpoint
+      print('./model'+self.modelnum)
+      ckpt = tf.train.get_checkpoint_state('./model'+self.modelnum+'/solid/')
+      print("Load model parameters from %s" % ckpt.model_checkpoint_path)
+      self.testmodel.saver.restore(self.testsess, ckpt.model_checkpoint_path)
+
+
+  def eval(self):
+    """ Randomly get a batch of data and output predictions """  
+    #inputs,enc_input_weights, outputs, dec_input_weights = self.get_test_batch()
+    count = 0
+    ans = None
+    beams = []
+    for input_,enc_input_weights_ in zip(self.test_inputs,self.test_enc_input_weights):
+      try:
+        predicted_ids = self.testmodel.step(self.testsess, [input_], [enc_input_weights_])
+      except Exception as err:
+        print(err)
+        continue
+      count += 1
+      print("="*20)
+      #print("inputs: ",inputs[i])
+      #query = []
+      #for x in input_:
+      #  if x[0] == -1:
+      #    break
+      #  query.append(self.id2word[str(x[0])])   
+      #print("input query: ", ' '.join(query))
+      #print("* %dth sample target: %s" % (i,str(outputs[i,:]-2)))
+      beams = []
+      for predict in predicted_ids[0]:
+        print("predict:",predict)
+        predquery = []
+        for x in predict:
+          if x > 0:
+            try:
+                predquery.append(self.id2word[str(input_[x-1][0])])
+            except Exception as err:
+                print(err)
+        ans = ' '.join(predquery)
+        beams.append(ans)
+        print("prediction: ", ans)
+    return beams
+
+  def correct(self,query):
+    tokens = query.split(' ')
+    try:
+        tokenids = [self.word2id[x] for x in tokens]
+    except Exception as err:
+        print(err)
+        return ''
+    tokenids.append(-1)
+    for x in self.id2word.keys():
+      if int(x) in tokenids:
+        continue
+      else:
+        tokenids.append(int(x))
+    self.vectorise(tokenids)
+    ans = self.eval()
+    return ans
+
+if __name__ == "__main__":
+  c = ConvexHull()
+  inputquery = 'select distinct ?vr0 where { <entpos@@1> <predpos@@1> ?vr0 }' 
+  beams = c.correct(inputquery)
+  print("input: ", inputquery)
+  for ans in beams:
+    print("corec: ",ans)
